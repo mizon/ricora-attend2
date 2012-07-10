fa0ffa28d98ae006953bd93d41efa9a0a5a6a79d
mizon <mizon9@gmail.com>
2012-07-11 02:30:11 +0900

    add debug log

--- ricora-atnd2.hs
+++ ricora-atnd2.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 import qualified Network.Wai.Handler.CGI as WCGI
 import RicoraAtnd2
+import Control.Exception
+import System.IO
+import Prelude hiding (catch)
 
 main :: IO ()
-main = WCGI.run =<< loadApp "./ricora-atnd2.yml"
+main = (WCGI.run =<< loadApp "./ricora-atnd2.yml") `catch` \(e :: SomeException) ->
+    bracket (openFile "./debug.log" AppendMode) hClose $ flip hPrint e
