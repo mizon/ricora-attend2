77e222581e602a6e3ca848df5a55d733ca87e07a
mizon <mizon9@gmail.com>
2012-07-28 17:27:16 +0900

    fix conf and debug log pathes

--- ricora-atnd2.hs
+++ ricora-atnd2.hs
@@ -3,8 +3,14 @@ import qualified Network.Wai.Handler.CGI as WCGI
 import RicoraAtnd2
 import Control.Exception
 import System.IO
+import qualified System.Directory as D
+import System.FilePath
+import Control.Applicative
 import Prelude hiding (catch)
 
 main :: IO ()
-main = (WCGI.run =<< loadApp "./ricora-atnd2.yml") `catch` \(e :: SomeException) ->
-    bracket (openFile "./debug.log" AppendMode) hClose $ flip hPrint e
+main = do
+    conf <- (</> ".ricora-atnd2.yml") <$> D.getHomeDirectory
+    debug <- (</> "ricora-atnd2.debug") <$> D.getHomeDirectory
+    (WCGI.run =<< loadApp conf) `catch` \(e :: SomeException) ->
+        bracket (openFile debug AppendMode) hClose $ flip hPrint e
